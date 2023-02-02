c410ca5a16589d776630c2337ba3eb4c9648fa42
asfand687 <asfandyar687@gmail.com>
2023-02-03 00:07:03 +0500

    added zustand values to drawer

--- src/Components/Blog/BlogNav.jsx
+++ src/Components/Blog/BlogNav.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { HiOutlineMenu, HiOutlineX } from "react-icons/hi"
 import Drawer from './Drawer'
 import { Link } from 'react-router-dom'
+import useStore from '../../store/ZustandStore'
 
-const BlogNav = ({ setFilter, isLight }) => {
-  const [showDrawer, setShowDrawer] = useState(false)
+const BlogNav = ({ isLight }) => {
+  const showDrawer = useStore((state) => state.showDrawer)
+  const toggleDrawer = useStore((state) => state.toggleDrawer)
   return (
     <section className="blogNav">
-      <Drawer setFilter={setFilter} showDrawer={showDrawer} setShowDrawer={setShowDrawer} />
+      <Drawer />
       <div className="blognav-container">
         <Link to="/home">
           <img src='/Icons/FinalLogo.jpg' alt="Incendiary Balloons" />
         </Link>
-        <div onClick={() => setShowDrawer(!showDrawer)}>
+        <div onClick={() => toggleDrawer()}>
           {
-            showDrawer ? <HiOutlineX className="blognav-icon" /> : <HiOutlineMenu className={`blognav-icon ${isLight ? 'text-white' : ''}`} />
+            showDrawer ?
+              <HiOutlineX className="blognav-icon" /> :
+              <HiOutlineMenu
+                className={`blognav-icon ${isLight ? 'text-white' : ''}`}
+              />
           }
         </div>
       </div>
