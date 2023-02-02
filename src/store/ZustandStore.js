e2d6d949bb1217e70a3d00e964a8dca3b65a39f7
asfand687 <asfandyar687@gmail.com>
2023-02-03 00:09:12 +0500

    added new state values to zustand store

--- src/store/ZustandStore.js
+++ src/store/ZustandStore.js
@@ -1,8 +1,13 @@
-import create from "zustand";
+import create from "zustand"
 
 const useStore = create((set) => ({
+    showDrawer: false,
+    toggleDrawer: () => set((state) => ({ showDrawer: !state.showDrawer })),
+    hideDrawer: () => set({ showDrawer: false }),
     visibleVideo: false,
     toggleVideo: () => set((state) => ({ visibleVideo: !state.visibleVideo })),
-    
-}));
+    blogCategory: "All",
+    setBlogCategory: (category) => set((state) => ({ blogCategory: category }))
+}))
+
 export default useStore;
