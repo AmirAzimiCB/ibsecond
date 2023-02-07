a266b111dbdb004c663da7b13b6116952a23702d
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2023-02-08 02:53:39 +0500

    Fixed Changes

--- src/Components/Blog/BlogPost.jsx
+++ src/Components/Blog/BlogPost.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
-import './blog.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./blog.css";
 
 const BlogPost = ({ src, slug, heading, body }) => {
-    return (
-        <article className="blog-post">
-            <article>
-            <img src={src} alt={heading}/> 
-            </article>
-            <article className="blog-post-text">
-                <Link to={slug}>
-                    <h2>{heading}</h2>
-                </Link>
-            </article>
-        </article>
-    )
-}
+  return (
+    <article className='blog-post'>
+      <article>
+        <img src={src} alt={heading} />
+      </article>
+      <article className='blog-post-text'>
+        <Link to={slug}>
+          <h2>{heading}</h2>
+        </Link>
+      </article>
+    </article>
+  );
+};
 
-
-export default BlogPost
+export default BlogPost;
--- src/Components/Blog/Drawer.jsx
+++ src/Components/Blog/Drawer.jsx
@@ -6,6 +6,8 @@ import useStore from "../../store/ZustandStore";
 import VideoLoader from "../VideoLoader/VideoLoader";
 
 const Drawer = ({ showDrawer, setFilter, setShowDrawer }) => {
+  const navigate = useNavigate();
+  const [navigateTo, setNavigateTo] = useState(null);
   const { categories } = useCategories();
   const changeCategory = (category) => {
     setShowDrawer(false);
@@ -15,80 +17,88 @@ const Drawer = ({ showDrawer, setFilter, setShowDrawer }) => {
       setFilter(category);
     }
   };
+  const handleClick = (link) => {
+    setNavigateTo(link);
+    // setShow(false);
+    if (window.innerWidth < 768) {
+      navigate(link);
+    } else {
+      useStore.setState({ showVideo: true });
+      setShowDrawer(false);
+    }
+  };
   return (
-    <div className={`drawer ${showDrawer ? "show" : "hide"}`}>
-      <section className='center-content h-full w-full nav-wrapper'>
-        <ul className='drawer-nav-links flex-1'>
-          <li>
-            <h4>01</h4>
-            <div className='flex justify-between items-center'>
-              <Link to='/home'>
+    <>
+      <div className={`drawer ${showDrawer ? "show" : "hide"}`}>
+        <section className='center-content h-full w-full nav-wrapper'>
+          <ul className='drawer-nav-links flex-1'>
+            <li onClick={() => handleClick("/home")}>
+              <h4>01</h4>
+              <div className='flex pointer justify-between items-center'>
                 <h2 className='font-cloister-black'>Home</h2>
-              </Link>
-              <FiArrowUpRight className='icon' />
-            </div>
-          </li>
-          <li>
-            <h4>02</h4>
-            <div className='flex justify-between items-center'>
-              <Link to='/manifesto'>
-                <h2 className='font-cloister-black'>Manifesto</h2>
-              </Link>
-              <FiArrowUpRight className='icon' />
-            </div>
-          </li>
-          <li>
-            <h4>03</h4>
-            <div className='flex justify-between items-center'>
-              <Link to='/founder'>
-                <h2 className='font-cloister-black'>Founder & CEO</h2>
-              </Link>
-              <FiArrowUpRight className='icon' />
-            </div>
-          </li>
-          <li>
-            <h4>04</h4>
-            <div className='flex justify-between items-center'>
-              <Link to='/protestresources'>
-                <h2 className='font-cloister-black'>Protest Resources</h2>
-              </Link>
-              <FiArrowUpRight className='icon' />
-            </div>
-          </li>
-          <li>
-            <h4>05</h4>
-            <div className='flex justify-between items-center'>
-              <Link to='/contact'>
-                <h2 className='font-cloister-black'>Contact</h2>
-              </Link>
-              <FiArrowUpRight className='icon' />
-            </div>
-          </li>
-        </ul>
 
-        <ul className='flex-2'>
-          <div className='flex justify-between items-center border-b-1 mb-2'>
-            <Link to='/blog'>
-              <h2 className='font-cloister-black'>News</h2>
-            </Link>
-            <FiArrowUpRight className='icon' />
-          </div>
-          <div className='flex'>
-            <article className='category-grid bg-white'>
-              {categories.map((category) => (
-                <p
-                  onClick={() => changeCategory(category.title)}
-                  key={category._id}
-                >
-                  {category.title}
-                </p>
-              ))}
-              <p onClick={() => changeCategory("All")}>All</p>
-            </article>
-          </div>
-        </ul>
-      </section>
-    </div>
+                <FiArrowUpRight className='icon' />
+              </div>
+            </li>
+            <li onClick={() => handleClick("/manifesto")}>
+              <h4>02</h4>
+              <div className='flex pointer justify-between items-center'>
+                <h2 className='font-cloister-black'>Manifesto</h2>
+
+                <FiArrowUpRight className='icon' />
+              </div>
+            </li>
+            <li onClick={() => handleClick("/founder")}>
+              <h4>03</h4>
+              <div className='flex pointer justify-between items-center'>
+                <h2 className='font-cloister-black'>Founder & CEO</h2>
+
+                <FiArrowUpRight className='icon' />
+              </div>
+            </li>
+            <li onClick={() => handleClick("/protestresources")}>
+              <h4>04</h4>
+              <div className='flex pointer justify-between items-center'>
+                <h2 className='font-cloister-black'>Protest Resources</h2>
+
+                <FiArrowUpRight className='icon' />
+              </div>
+            </li>
+            <li onClick={() => handleClick("/contact")}>
+              <h4>05</h4>
+              <div className='flex pointer justify-between items-center'>
+                <h2 className='font-cloister-black'>Contact</h2>
+
+                <FiArrowUpRight className='icon' />
+              </div>
+            </li>
+          </ul>
+
+          <ul className='flex-2'>
+            <div className='flex justify-between items-center border-b-1 mb-2'>
+              <Link to='/blog'>
+                <h2 className='font-cloister-black'>News</h2>
+              </Link>
+              <FiArrowUpRight className='icon' />
+            </div>
+            <div className='flex'>
+              <article className='category-grid bg-white'>
+                {categories.map((category) => (
+                  <p
+                    onClick={() => changeCategory(category.title)}
+                    key={category._id}
+                  >
+                    {category.title}
+                  </p>
+                ))}
+                <p onClick={() => changeCategory("All")}>All</p>
+              </article>
+            </div>
+          </ul>
+        </section>
+      </div>
+      {navigateTo && <VideoLoader Navigateto={navigateTo} />}
+    </>
   );
 };
 
