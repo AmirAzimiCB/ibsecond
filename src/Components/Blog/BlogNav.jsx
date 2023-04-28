e98596b8c91a82910c08ecb61bd8562724cc5f55
Amir A <amir-azimi@hotmail.com>
2023-04-28 17:11:59 +0500

    add new div
    add new div

--- src/Components/Blog/BlogNav.jsx
+++ src/Components/Blog/BlogNav.jsx
@@ -6,24 +6,26 @@ import { Link } from "react-router-dom";
 const BlogNav = ({ setFilter, isLight }) => {
   const [showDrawer, setShowDrawer] = useState(false);
   return (
-    <section className='blogNav' style={{ zIndex: 10000 }}>
-      <Drawer
-        setFilter={setFilter}
-        showDrawer={showDrawer}
-        setShowDrawer={setShowDrawer}
-      />
-      <div className='blognav-container'>
-        <Link to='/home'>
-          <img src='/Icons/FinalLogo.jpg' alt='Incendiary Balloons' />
-        </Link>
-        <div onClick={() => setShowDrawer(!showDrawer)}>
-          {showDrawer ? (
-            <HiOutlineX className='blognav-icon' />
-          ) : (
-            <HiOutlineMenu
-              className={`blognav-icon ${isLight ? "text-white" : ""}`}
-            />
-          )}
+    <section className="blogNav" style={{ zIndex: 10000 }}>
+      <div className="__block">
+        <Drawer
+          setFilter={setFilter}
+          showDrawer={showDrawer}
+          setShowDrawer={setShowDrawer}
+        />
+        <div className="blognav-container">
+          <Link to="/home">
+            <img src="/Icons/FinalLogo.jpg" alt="Incendiary Balloons" />
+          </Link>
+          <div onClick={() => setShowDrawer(!showDrawer)}>
+            {showDrawer ? (
+              <HiOutlineX className="blognav-icon" />
+            ) : (
+              <HiOutlineMenu
+                className={`blognav-icon ${isLight ? "text-white" : ""}`}
+              />
+            )}
+          </div>
         </div>
       </div>
     </section>
