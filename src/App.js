bc53add87ebe3c46e48009b1f7b92330c0536b11
Amir A <amir-azimi@hotmail.com>
2023-05-06 01:52:48 +0600

    Fixed more styling issues as well as routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import BlogPost from "./Components/Blog/BlogPost"
-import BlogShow from "./Components/Blog/BlogShow"
-import VirtualGallery from "./Components/VirtualGallery/VirtualGallery"
-import About from "./Pages/About"
-import Blog from "./Pages/Blog"
-import BlogFilter from "./Pages/BlogFilter"
-import Contact from "./Pages/Contact"
-import Cover from "./Pages/Cover"
-import Founder from "./Pages/Founder"
-import Home from "./Pages/Home"
-import Post from "./Pages/Post"
-import ProtestResources from "./Pages/ProtestResources"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BlogPost from "./Components/Blog/BlogPost";
+import BlogShow from "./Components/Blog/BlogShow";
+import VirtualGallery from "./Components/VirtualGallery/VirtualGallery";
+import About from "./Pages/About";
+import Blog from "./Pages/Blog";
+import BlogFilter from "./Pages/BlogFilter";
+import Contact from "./Pages/Contact";
+import Cover from "./Pages/Cover";
+import Founder from "./Pages/Founder";
+import Home from "./Pages/Home";
+import Post from "./Pages/Post";
+import ProtestResources from "./Pages/ProtestResources";
+import "./App.scss";
 
-const App=()=>{
-    return (
-        <>
-        <BrowserRouter>
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Cover/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/manifesto" element={<About/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/:category" element={<BlogFilter/>}/>
-        <Route path="/founder" element={<Founder/>}/>
-        <Route path="/gallery" element={<VirtualGallery/>}/>
-        <Route path="/protestresources" element={<ProtestResources/>}/>
-        <Route path="/blog" element={<Blog/>}/>
-        <Route path="/blog/:slug" element={<Post/>}/>
+          <Route path="/" element={<Cover />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/manifesto" element={<About />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/:category" element={<BlogFilter />} />
+          <Route path="/founder" element={<Founder />} />
+          <Route path="/gallery" element={<VirtualGallery />} />
+          <Route path="/protestresources" element={<ProtestResources />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<Post />} />
         </Routes>
-        </BrowserRouter>
-        </>
-    )
-}
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/HomeImages.jsx
+++ src/HomeImages.jsx
@@ -5,20 +5,23 @@ import "./HomeImages.css";
 export default function HomeImages() {
   const descRefTop = React.useRef(null);
 
-
   return (
-    <div className='imagesContainer' id='imagesContainer'>
-      <img className='homeBg' src='/Images/Home-bg.png' alt='1' border='0' />
-      <div className='mainContainer'>
-        <div className='left-img-container'>
+    <div
+      style={{ zIndex: 9999 }}
+      className="imagesContainer"
+      id="imagesContainer"
+    >
+      <img className="homeBg" src="/Images/Home-bg.png" alt="1" border="0" />
+      <div className="mainContainer">
+        <div className="left-img-container">
           <img
-            className='left-img'
-            src='/Images/Home-img-left.png'
-            alt='1'
-            border='0'
+            className="left-img"
+            src="/Images/Home-img-left.png"
+            alt="1"
+            border="0"
           />
-          <div className='home-desc-text' id='home-desc-text'>
-            <span ref={descRefTop} id='topText' className='top-text'>
+          <div className="home-desc-text" id="home-desc-text">
+            <span ref={descRefTop} id="topText" className="top-text">
               Welcome to Incendiary Balloons! We are a One-Of-A-Kind Documentary
               News Platform Televising the Revolution and Reshaping the Media's
               Narrative Of Our Communities. You are about to enter our 3D Art
@@ -34,59 +37,49 @@ export default function HomeImages() {
             </span>
           </div>
         </div>
-        <div className='right-img-container'>
-          <div className='right-img-inner-upper-container'>
-            <Link to='/gallery'>
-            <div
-              
-              className='right-img-inner-upper'
-              >
-              <img
-                src='/Images/Home-img1.png'
-                alt='1'
-                border='0'
-                className='home-img upper-right-img'
+        <div className="right-img-container">
+          <div className="right-img-inner-upper-container">
+            <Link to="/gallery">
+              <div className="right-img-inner-upper">
+                <img
+                  src="/Images/Home-img1.png"
+                  alt="1"
+                  border="0"
+                  className="home-img upper-right-img"
                 />
-              <div className='home-text-container'>
-                <span className='home-text-container-span'>ENTER</span>
+                <div className="home-text-container">
+                  <span className="home-text-container-span">ENTER</span>
+                </div>
               </div>
-            </div>
-                </Link>
-            <Link to='/gallery'>
-            <div
-              
-              className='right-img-inner-upper'
-            >
-              <img
-                src='/Images/Home-img2.png'
-                alt='1'
-                className='home-img upper-right-img'
-                border='0'
-              />
-              <div className='home-text-container'>
-                <span className='home-text-container-span'>THE</span>
-              </div>
-            </div>
             </Link>
-              
+            <Link to="/gallery">
+              <div className="right-img-inner-upper">
+                <img
+                  src="/Images/Home-img2.png"
+                  alt="1"
+                  className="home-img upper-right-img"
+                  border="0"
+                />
+                <div className="home-text-container">
+                  <span className="home-text-container-span">THE</span>
+                </div>
+              </div>
+            </Link>
           </div>
-            <Link to='/gallery'>
-          <div
-            
-            className='right-img-inner-lower-container'
-          >
-            <div className='right-img-inner-lower'>
-              <img
-                src='/Images/Home-img3.png'
-                className='home-img'
-                alt='1'
-                border='0'
-              />
-              <div className='home-text-container'>
-                <span className='home-text-container-span'>GALLERY</span>
+          <Link to="/gallery">
+            <div className="right-img-inner-lower-container">
+              <div className="right-img-inner-lower">
+                <img
+                  src="/Images/Home-img3.png"
+                  className="home-img"
+                  alt="1"
+                  border="0"
+                />
+                <div className="home-text-container">
+                  <span className="home-text-container-span">GALLERY</span>
+                </div>
               </div>
             </div>
-          </div>
           </Link>
         </div>
       </div>
