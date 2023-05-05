8e0be8604ee99ed6dbf3bed3cc8733943c24b978
Amir A <amir-azimi@hotmail.com>
2023-05-06 02:25:53 +0600

    reuploaded

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -10,14 +10,15 @@ import Modal from "react-modal";
 import BlogShow from "../Components/Blog/BlogShow";
 import BlogPopup from "./Blogpopup";
 
-
 const Blog = () => {
-  const blogCategory = useStore((state) => state.blogCategory)
-  const { filteredPosts } = usePosts(blogCategory)
+  const blogCategory = useStore((state) => state.blogCategory);
+  const { filteredPosts } = usePosts(blogCategory);
   const [showPopup, setShowPopup] = useState(false);
-  const styling={
-    backgroundColor:'BLACK',':hover':{backgroundColor:'yellow'}, cursor: "pointer",
-  }
+  const styling = {
+    backgroundColor: "BLACK",
+    ":hover": { backgroundColor: "yellow" },
+    cursor: "pointer",
+  };
 
   const handleOpenPopup = () => {
     setShowPopup(true);
@@ -25,52 +26,52 @@ const Blog = () => {
 
   const handleClosePopup = () => {
     setShowPopup(false);
-  }
-  useEffect(() => {
-  }, [blogCategory])
+  };
+  useEffect(() => {}, [blogCategory]);
   return (
     <>
-    <Helmet>
-        <title>
-          Blog
-        </title>
+      <Helmet>
+        <title>Blog</title>
       </Helmet>
-         <div className="blog-page">
-      <BlogNav />
-      <div className="main font-helvetica blog-posts-container">
-      {
-          filteredPosts.length ? (
+      <div className=" blog-nav-fixed">
+        <BlogNav />
+        <div className="main font-helvetica blog-posts-container">
+          {filteredPosts.length ? (
             <>
               <section className="blog-header flex pb-10">
                 <article className="flex-1">
-                  <img className="spacing-0" src={filteredPosts[0]?.mainImage?.asset?.url} alt={filteredPosts[0?.title]} />
+                  <img
+                    className="spacing-0"
+                    src={filteredPosts[0]?.mainImage?.asset?.url}
+                    alt={filteredPosts[0?.title]}
+                  />
                 </article>
                 <article className="flex-1 bg-black text-white p-8 flex items-center">
                   <Link to={filteredPosts[0]?.slug.current}>
-                    <h2 className="text-white">
-                      {filteredPosts[0]?.title}
-                    </h2>
+                    <h2 className="text-white">{filteredPosts[0]?.title}</h2>
                   </Link>
                 </article>
               </section>
               <section>
                 <h5 className="blog-list-section__title">The Latest</h5>
-                {filteredPosts?.map((post, i) => i > 0 && (
-                  <BlogPost
-                    key={post.slug.current}
-                    slug={post.slug.current}
-                    src={post.mainImage?.asset?.url}
-                    heading={post.title}
-                  />
-                ))}
+                {filteredPosts?.map(
+                  (post, i) =>
+                    i > 0 && (
+                      <BlogPost
+                        key={post.slug.current}
+                        slug={post.slug.current}
+                        src={post.mainImage?.asset?.url}
+                        heading={post.title}
+                      />
+                    )
+                )}
               </section>
             </>
           ) : (
             <div>No Posts Found, please choose another category</div>
-          )
-        }
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
