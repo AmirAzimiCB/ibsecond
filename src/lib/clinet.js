67794569523a2082112fa88250b4568b2cada127
Amir A <amir-azimi@hotmail.com>
2023-05-07 02:23:08 +0600

    Add files via upload

--- src/lib/clinet.js
+++ src/lib/clinet.js
@@ -1,8 +1,13 @@
-import client from "@sanity/client";
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
-export default client({
+export const client = sanityClient({
   projectId: "gwaghe3o",
   dataset: "post",
-  useCdn: true,
   apiVersion: "2021-10-21",
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
