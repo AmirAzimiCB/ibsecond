31d98baaa5404aaa5d9a0fbce9493f30aa553f84
Amir A <amir-azimi@hotmail.com>
2023-05-06 11:49:28 +0600

    FIxed pages

--- src/Pages/BlogFilter.jsx
+++ src/Pages/BlogFilter.jsx
@@ -7,55 +7,55 @@ import { Link, useParams } from "react-router-dom";
 import useStore from "../store/ZustandStore";
 import { Helmet } from "react-helmet-async";
 import { useState } from "react";
-import { useLocation } from "react-router-dom"
-
+import { useLocation } from "react-router-dom";
 
 const BlogFilter = () => {
-  const location = useLocation()
-  let vars
-  const [state,setState]=useState(false)
-  const blogCategory = useStore((state) => state.blogCategory)
-  const { filteredPosts } = usePosts(blogCategory)
+  const location = useLocation();
+  let vars;
+  const [state, setState] = useState(false);
+  const blogCategory = useStore((state) => state.blogCategory);
+  const { filteredPosts } = usePosts(blogCategory);
   // console.log(filteredPosts[0]?.categories?.title)
-  const value=filteredPosts[0]?.categories?.title
-  const params=useParams()
-  for(var i=0;i<filteredPosts.length;i++){
-    if(params.category===filteredPosts[i]){
-      setState(true)
+  const value = filteredPosts[0]?.categories?.title;
+  const params = useParams();
+  for (var i = 0; i < filteredPosts.length; i++) {
+    if (params.category === filteredPosts[i]) {
+      setState(true);
     }
   }
-  console.log(blogCategory)
-  useEffect(() => {
-}, [blogCategory])
+  console.log(blogCategory);
+  useEffect(() => {}, [blogCategory]);
   return (
     <>
-    <Helmet>
-        <title>
-          Blog
-        </title>
+      <Helmet>
+        <title>Blog</title>
       </Helmet>
-         <div className="blog-page">
-      <BlogNav />
-      <div className="main font-helvetica blog-posts-container">
+      <div className="blog-page">
+        <BlogNav />
+        <div className="main font-helvetica blog-posts-container">
           <h5 className="blog-list-section__title">{blogCategory}</h5>
-        {
-            filteredPosts.length ? (
-              <section>
-                {filteredPosts?.map((post) => (
-                  post?.categories?.title === params.category ? (
-                    <BlogPost
-                      key={post.slug.current}
-                      slug={`/blog/${post.slug.current}`}
-                      src={post.mainImage?.asset?.url}
-                      heading={post.title}
-                      post={post}
-                    />) :
-                    (null)))
-                }
-              </section>) : (<div>No articles from <strong>{blogCategory}</strong> has been published yet, please choose another category</div>)
-        }
+          {filteredPosts.length ? (
+            <section>
+              {filteredPosts?.map((post) =>
+                post?.categories?.title === params.category ? (
+                  <BlogPost
+                    key={post.slug.current}
+                    slug={`/blog/${post.slug.current}`}
+                    src={post.mainImage?.asset?.url}
+                    heading={post.title}
+                    post={post}
+                  />
+                ) : null
+              )}
+            </section>
+          ) : (
+            <div>
+              No articles from <strong>{blogCategory}</strong> has been
+              published yet, please choose another category
+            </div>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom";
 import { PortableText } from "@portabletext/react";
-import BlockContent from '@sanity/block-content-to-react'
-import getYouTubeId from "get-youtube-id"
-import client from '../lib/clinet'
-import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from "@sanity/block-content-to-react";
+import getYouTubeId from "get-youtube-id";
+import client from "../lib/clinet";
+import imageUrlBuilder from "@sanity/image-url";
 
 import Layout from "../Components/Layout";
 import useSinglePost from "../hooks/useSinglePost";
@@ -11,50 +11,52 @@ import "../styles/post.css";
 import YouTube from "react-youtube";
 import BlogNav from "../Components/Blog/BlogNav";
 import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
 
 const Post = () => {
   const { slug } = useParams();
   const { post, loading } = useSinglePost(slug);
+  useEffect(() => {
+    window.location.reload();
+  }, [loading]);
 
   const serializers = {
     types: {
       youtube: ({ node }) => {
-        const { url } = node
-        const id = getYouTubeId(url)
+        const { url } = node;
+        const id = getYouTubeId(url);
 
-        return (<YouTube iframeClassName={"iframe"} videoId={id} />)
+        return <YouTube iframeClassName={"iframe"} videoId={id} />;
       },
       code: (props) => (
         <pre data-language={props.node.language}>
           <code>{props.node.code}</code>
         </pre>
       ),
-      image: props => {
+      image: (props) => {
         const { node } = props;
         const builder = imageUrlBuilder(client);
         const src = builder.image(node.asset._ref).url();
-        return <img src={src} alt={node.alt} />
-      }
-    }
-  }
+        return <img src={src} alt={node.alt} />;
+      },
+    },
+  };
 
   return (
     <>
-     <Helmet>
-        <title>
-          Post
-        </title>
+      <Helmet>
+        <title>Post</title>
       </Helmet>
       <BlogNav isLight />
       <div className="container font-helvetica blog-post__container">
         {loading && <div>Loading...</div>}
-        {post?.mainImage && <img loading="lazy" src={post?.mainImage?.asset?.url} alt={""} />}
+        {post?.mainImage && (
+          <img loading="lazy" src={post?.mainImage?.asset?.url} alt={""} />
+        )}
         <div className="post-content">
           <h1 className="post-title">{post?.title}</h1>
-          <h3 className="category">
-            {post?.categories?.title}
-          </h3>
-        <BlockContent blocks={post?.body} serializers={serializers} />
+          <h3 className="category">{post?.categories?.title}</h3>
+          <BlockContent blocks={post?.body} serializers={serializers} />
         </div>
       </div>
     </>
