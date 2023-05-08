bf03baf8d9ab41d3b82ab91a2c3fd9f14b958203
Amir A <amir-azimi@hotmail.com>
2023-05-09 00:32:59 +0600

    Add files via upload

--- src/Components/Blog/BlogNav.jsx
+++ src/Components/Blog/BlogNav.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 import Drawer from "./Drawer";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const BlogNav = ({ setFilter, isLight }) => {
   const [showDrawer, setShowDrawer] = useState(false);
+  const nav = useLocation();
+  useEffect(() => {
+    console.log(nav.pathname);
+  }, [nav.pathname]);
   return (
-    <section className="blogNav" style={{ zIndex: 10000 }}>
+    <section className="blogNav" style={{ zIndex: 100000000 }}>
       <div className={`__block padded_container ${showDrawer ? "active" : ""}`}>
         <div className="blogNavBar_container">
-          <Link to="/home">
+          {/* <Link to={`${nav.pathname === "/home" ? "/Home" : "/home"}`}> */}
+          <a href="/home">
             <img src="/Icons/FinalLogo.jpg" alt="Incendiary Balloons" />
-          </Link>
+          </a>
+          {/* </Link> */}
           <div
             className="drawer_control"
             onClick={() => setShowDrawer(!showDrawer)}
