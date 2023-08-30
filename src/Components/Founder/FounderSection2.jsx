c74c4ac77d3aeee1d68f6de5cc6f8909df3b9ce5
Amir A <amir-azimi@hotmail.com>
2023-08-30 22:01:25 +0600

    Update FounderSection2.jsx

--- src/Components/Founder/FounderSection2.jsx
+++ src/Components/Founder/FounderSection2.jsx
@@ -31,18 +31,7 @@ function FounderSection2() {
             marginLeft: window.innerWidth < 900 ? "0" : "2rem",
           }}
         >
-          “When I was little I used to dream I could change what I saw happening
-          in my community… what my neighborhood was going through. I hated the
-          narrative used to describe my home; what we would now consider to be
-          “expected” cultural canon. My passion, my activism, everything that
-          translates into my journalism, ever since I can remember, came as a
-          reflex out of forced circumstances. Seeing little-to-no authentic
-          representational viewpoints of Black women in the media, coupled with
-          agenda-based narrative, forced upon the Black community, as a whole,
-          within the news. That’s something I constantly work toward changing…
-          it’s definitely one of my top career and life goals. For me, it’s more
-          than trying to “objectify” the struggle that Black people face. It is
-          taking action.
+         Ever since I can remember, my art and my work came as a reflex to forced circumstances. As a young Black girl, I saw the relationship between media and Black livelihood as parasitic.  Seeing little-to-no authentic representational viewpoints of Black women in the media, coupled with agenda-based narrative, forced upon the Black community, as a whole, within the news. That’s something I constantly work toward changing… It's definitely one of my top career and life goals. My passion, my activism, ignited within me as a reflex, born from the crucible of adversities.My passion, my activism, an instinct etched into my very being by the cruelties that my family and my neighborhood endured.  I witnessed the dearth of authentic portrayals of Black women in the media, suffocated by biased agendas thrust upon the Black collective. A ceaseless battle, a topmost ambition that fuels my life and career. Each breath I draw, each step I take, propels me toward this mission.
         </span>
         <Scene
           position={window.innerWidth < 900 ? [0, 0, 0] : [0.4, -0.4, 0]}
@@ -51,15 +40,7 @@ function FounderSection2() {
       <div className=' founder-text-container'>
         <Scene position={window.innerWidth < 900 ? [0, 0, 0] : [-0.4, 0, 0]} />
         <span className='founder-text-bg founder3-text'>
-          We’ve been “objectified” and “commodified” by people who have
-          re-worked our narratives, our culture, to their advantage. I believe
-          now is the time to place the story, of the culture, back into the
-          hands of the culture. All of my documentaries are solely dedicated to
-          fighting for this, and only this. And that is something I am
-          constantly working toward and something I wake up everyday with intent
-          to do. To Fight. Fight for my truth, for my communities truth, for any
-          marginalized communities truth. OUR truth will to be heard. “ - Talia
-          Smith
+          My people have suffered from being objectified and commodified, our narratives and culture plundered, reworked to suit their advantage. Now, the time has come to reclaim our story, to restore it to the hands of our people. My lifelong goal has been to undo the heinous transmutation of our stories, our culture, that has been manipulated by profiteers who twist them to their advantage. Now, the time has come to wrest the narrative, the very essence of our culture, from their grasp.  Each documentary birthed from my soul is a battle cry for this singular cause. It is the unyielding commitment that stirs my waking hours, the resolve that propels me forward each day. It is my unyielding commitment, my daily purpose. To wage war. To fight for my truth, for the truth of my community, and for every community shackled by the plundering colonizers and the insidious machinations of economic and political systems designed to bolster their reign. My philosophy takes shape in a singular path, guided by these unwavering tenets: Identify. Disrupt & Dismantle. Rebuild. Distribute “- Talia Smith
         </span>
       </div>
     </div>
