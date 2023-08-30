acefd3a93cf6e48a4f804de1ca3c98ad2294a6d5
Amir A <amir-azimi@hotmail.com>
2023-08-30 22:00:25 +0600

    Update FounderSection3.jsx

--- src/Components/Founder/FounderSection3.jsx
+++ src/Components/Founder/FounderSection3.jsx
@@ -23,22 +23,10 @@ function FounderSection3() {
             marginTop: window.innerWidth < 900 ? "5rem" : "0",
           }}
         >
-          Coming up without any industry connections, Talia’s persistence
-          allowed her to knock-down doors at major news outlets; that were not
-          readily open to her cultural point of view. Talia’s work has appeared
-          in Harper’s Bazaar, Cheddar TV, Paper Magazine, Vice, Nylon Magazine,
-          Dazed & Confused, GALORE, The New York Observer, among many other
-          places. Talia continues to persevere to ensure the that the culture is
-          represented on increasingly bigger platforms, and that the revolution
-          WILL be televised.
+         Coming up without any industry connections, Talia’s persistence allowed her to knock-down doors at major news outlets; that were not readily open to her cultural point of view. Talia’s work has appeared in The New York Times, ELLE Magazine, Harper’s Bazaar, ALLURE Buzzfeed, Cheddar TV, Paper Magazine, Vice, Nylon Magazine, Dazed & Confused, GALORE, The New York Observer, among many other places. Talia continues to persevere to ensure that the culture is represented on increasingly bigger platforms, and that the revolution WILL be televised.
           <br />
           <br />
-          Talia holds undergraduate degree in journalism from The Arthur L.
-          Carter Institute at New York University and was heavily inspired by a
-          long list of readings by James Baldwin, Audre Lorde and more. Talia
-          describes her philosophy as a mixture of all her idols Tupac Shakur,
-          Hayao Miyazaki, Will Smith, Frida Kahlo, Sandra Cisneros and strong
-          women, like her mother.
+Talia is heavily inspired by a long list of readings by James Baldwin, Audre Lorde and more. Talia describes her philosophy as a mixture of all her idols Tupac Shakur, Hayao Miyazaki, Will Smith, Frida Kahlo, Sandra Cisneros and strong women, like her mother.
         </span>
         <Scene position={window.innerWidth < 900 ? [0, 0.5, 0] : [0.5, 0, 0]} />
       </div>
